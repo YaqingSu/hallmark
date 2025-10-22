f41966fd1dd08d0206793d4286df1a9a35416cdf
rohinsant <rohin999.s@gmail.com>
2025-10-22 16:24:46 -0700

    Changed core file so that it becomes a subclass of pandas dataframe. Added a class method to parse function

--- mod/hallmark/core.py
+++ mod/hallmark/core.py
@@ -22,15 +22,43 @@ import pandas as pd
 import numpy as np
 
 class ParaFrame(pd.DataFrame):
-    # @property
-    # def _constructor(self):
-    #     return ParaFrame
+    @property
+    def _constructor(self):
+        return ParaFrame
 
-    # def __init__(self, fmt):
-    #     self.fmt = fmt
-    #     return ParaFrame
+    def __call__(self, **kwds):
+        return self.filter(**kwds)
     
-    def Parse(self, *args, debug=False, **kwargs):
+    def filter(self, **kwargs):
+        """Filter a pandas ``DataFrame`` by matching column values.
+
+        This function is monkey-patched onto :class:`pandas.DataFrame` as
+        ``__call__``.
+            It allow direct filtering of DataFrames with keyword arguments.
+
+        Args:
+         **kwargs: Arbitrary keyword arguments specifying column names
+             and values to filter by.
+            * If the value is a tuple or list, rows where the column
+               matches any of those values are selected.
+            * If the value is a scalar, rows where the column equals
+               the value are selected.
+
+        Returns:
+         pandas.DataFrame: A filtered DataFrame containing only rows
+             that match the given conditions.
+
+        """
+        mask = [False] * len(self)
+        for k, v in kwargs.items():
+            if isinstance(v, (tuple, list)):
+                mask |= np.isin(np.array(self[k]),np.array(v))
+            else:
+                mask |= np.array(self[k]) == v
+        return self[mask]
+
+    @classmethod
+    def parse(cls, fmt, *args, debug=False, **kwargs):
         """Construct a ``ParaFrame`` by parsing file paths that match a pattern.
 
         This function searches for files whose names match a formatted
@@ -41,38 +69,39 @@ class ParaFrame(pd.DataFrame):
         DataFrame.
 
         Args:
-            fmt (str): A format string specifying the expected file naming
-                pattern.
-                Fields wrapped in ``{}`` will be extracted into columns.
-            *args: Positional arguments used to fill the format string.
-            debug (bool, optional): If True, prints debugging information
-                about the matching process.
-                Defaults to False.
-            **kwargs: Keyword arguments used to fill the format string.
-                If missing keys are encountered, they will be replaced by
-                a wildcard ``*`` for globbing.
+        fmt (str): A format string specifying the expected file naming
+            pattern.
+            Fields wrapped in ``{}`` will be extracted into columns.
+        *args: Positional arguments used to fill the format string.
+        debug (bool, optional): If True, prints debugging information
+            about the matching process.
+            Defaults to False.
+        **kwargs: Keyword arguments used to fill the format string.
+            If missing keys are encountered, they will be replaced by
+            a wildcard ``*`` for globbing.
 
         Returns:
-            pandas.DataFrame: A DataFrame where each row corresponds to a
-                matched file.
-                Includes:
-                * ``path``: the full file path
-                * additional columns extracted from the format fields
+        pandas.DataFrame: A DataFrame where each row corresponds to a
+        matched file.
+        Includes:
+        * ``path``: the full file path
+        * additional columns extracted from the format fields
 
         Example:
-            >>> from hallmark import ParaFrame
-            >>> pf = ParaFrame("data/run{run:d}_p{parameter:d}.csv")
-            >>> print(pf)
-            path               run parameter
-            0  data/run1_p10.csv  1   10
-            1  data/run2_p20.csv  2   20
-        """
-        pmax = len(self.fmt) // 3  # to specify a parameter, we need at least
-                            # three characters '{p}'; the maximum number
-                            # of possible parameters is `len(fmt) // 3`.
+        >>> from hallmark import ParaFrame
+        >>> pf = ParaFrame("data/run{run:d}_p{parameter:d}.csv")
+        >>> print(pf)
+           path               run parameter
+        0  data/run1_p10.csv  1   10
+        1  data/run2_p20.csv  2   20
 
-        # Construct the glob pattern for search files
-        pattern = self.fmt
+        """
+        pmax = len(fmt) // 3  # to specify a parameter, we need at least
+                          # three characters '{p}'; the maximum number
+                          # of possible parameters is `len(fmt) // 3`.
+
+                        # Construct the glob pattern for search files
+        pattern = fmt
         for i in range(pmax):
             if debug:
                 print(i, pattern, args, kwargs)
@@ -99,7 +128,7 @@ class ParaFrame(pd.DataFrame):
                 print(f'No match; please check format string')
 
         # Parse list of file names back to parameters
-        parser = parse.compile(self.fmt)
+        parser = parse.compile(fmt)
 
         l = []
         for f in files:
@@ -108,33 +137,4 @@ class ParaFrame(pd.DataFrame):
                 print(f'Failed to parse "{f}"')
             else:
                 l.append({'path':f, **r.named})
-        return ParaFrame
-    
-    def filter(self, **kwargs):
-        """Filter a pandas ``DataFrame`` by matching column values.
-
-        This function is monkey-patched onto :class:`pandas.DataFrame` as
-        ``__call__``.
-        It allow direct filtering of DataFrames with keyword arguments.
-
-        Args:
-            **kwargs: Arbitrary keyword arguments specifying column names
-                and values to filter by.
-                * If the value is a tuple or list, rows where the column
-                matches any of those values are selected.
-                * If the value is a scalar, rows where the column equals
-                the value are selected.
-
-        Returns:
-            pandas.DataFrame: A filtered DataFrame containing only rows
-                that match the given conditions.
-
-        """
-        mask = [False] * len(self)
-        for k, v in kwargs.items():
-            if isinstance(v, (tuple, list)):
-                mask |= np.isin(np.array(self[k]),np.array(v))
-            else:
-                mask |= np.array(self[k]) == v
-        return self[mask]
-
+        return cls(l)
